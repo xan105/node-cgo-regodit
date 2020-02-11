b06bad713aad63acc7197f7a02a1adcbd9c53b77
Anthony Beaumont <dev.xan105@outlook.be>
2020-02-11 19:05:54 +0700

    add ffi async inside promises

--- regedit.js
+++ regedit.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const ffi = require('ffi-napi');
 
-const regedit = ffi.Library(path.resolve(__dirname, "build/regedit.dll"), {
+const lib = ffi.Library(path.join(__dirname, "build",`${(process.arch === "x64") ? 'regedit.x64.dll' : 'regedit.x86.dll'}`), {
    'RegKeyExists': ["int", ["string", "string"]],
    'RegListAllSubkeys': ["string", ["string", "string"]],
    'RegListAllValues': ["string", ["string", "string"]],
@@ -21,138 +21,234 @@ const regedit = ffi.Library(path.resolve(__dirname, "build/regedit.dll"), {
 });
 
 const HKEY = ["HKCR","HKCU","HKLM","HKU","HKCC"];
+function goPath(s) { return s.replace(/([\/])/g,"\\") }
 
-function goPath(p) {
-  return p.replace(/([\/])/g,"\\");
-}
-
-module.exports.RegKeyExists = function(root,key) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-    
-    let result = regedit.RegKeyExists(root,goPath(key));
-    
-    return result === 1 ? true : false;
-  
-}
-
-module.exports.RegListAllSubkeys = function(root,key) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    let list = regedit.RegListAllSubkeys(root,goPath(key));
-    
-    if (list) {
-    
-      let result = list.split(",");
-      
-      return (result.length > 0) ? result : null;
-      
-    } else {
-      return null;
-    }
-  
-}
-
-module.exports.RegListAllValues = function(root,key) {
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    let list = regedit.RegListAllValues(root,goPath(key));
-    
-    if (list) {
-    
-      let result = list.split(",");
-      
-      if (result.length > 0) {
-        
-        return result.sort(function(a, b){ //Alphabetical sort to match regedit view
-            if(a < b) { return -1; }
-            if(a > b) { return 1; }
-            return 0;
-        })
-        
-      } else {
-        return null;
+module.exports = {
+    promises : {
+      RegKeyExists : (root,key) => {
+        return new Promise((resolve,reject) => {
+          if (!HKEY.some(key => root === key)) return reject(`Unvalid root key "${root}"`);
+          lib.RegKeyExists.async(root,goPath(key), (err, res) => {
+            if(err) return reject(err);
+            return resolve((res === 1) ? true : false);
+          });
+        });  
+      },
+      RegListAllSubkeys : (root,key) => {
+        return new Promise((resolve,reject) => {
+          if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+          lib.RegListAllSubkeys.async(root,goPath(key), (err, list) => {
+            if(err) return reject(err);
+            if(list) {
+              let res = list.split(",");
+              return resolve((res.length > 0) ? res : null);
+            } else {
+              return resolve(null);
+            }
+          });
+        });
+      }, 
+      RegListAllValues : (root,key) => {
+        return new Promise((resolve,reject) => {
+          if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+          lib.RegListAllValues.async(root,goPath(key), (err, list) => {
+            if(err) return reject(err);
+            if(list) {
+              let res = list.split(",");
+              if (res.length > 0) { 
+                return resolve(res.sort(function(a, b){ //Alphabetical sort to match regedit view
+                    if(a < b) { return -1; }
+                    if(a > b) { return 1; }
+                    return 0;
+                }));
+              } else {
+                return resolve(null);
+              }
+            } else {
+              return resolve(null);
+            }
+          });
+        });
+      },
+      RegQueryStringValue : (root,key,name) => { // REG_SZ & REG_EXPAND_SZ
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`; 
+            let res = lib.RegQueryStringValue.async(root,goPath(key),name, (err, res) => {
+              if(err) return reject(err);
+              return resolve((res) ? res : null);
+            });
+        });
+      },
+      RegQueryStringValueAndExpand : (root,key,name) => { // REG_EXPAND_SZ (expands environment-variable strings)
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`; 
+            let res = lib.RegQueryStringValueAndExpand.async(root,goPath(key),name, (err, res) => {
+              if(err) return reject(err);
+              return resolve((res) ? res : null);
+            });
+        });
+      },
+      RegQueryBinaryValue : (root,key,name) => { //REG_BINARY
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;  
+            let res = lib.RegQueryBinaryValue.async(root,goPath(key),name, (err, res) => {
+              if(err) return reject(err);
+              return resolve((res) ? res : null);
+            });
+        });
+      },
+      RegQueryIntegerValue : (root,key,name) => { //REG_DWORD & REG_QWORD
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;   
+            let res = lib.RegQueryIntegerValue.async(root,goPath(key),name, (err, res) => {
+              if(err) return reject(err);
+              return resolve((res) ? res : null);
+            });
+        });
+      },
+      RegWriteStringValue : (root,key,name,value) => {
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+            lib.RegWriteStringValue.async(root,goPath(key),name,value.toString(), (err, res) => {
+              if(err) return reject(err);
+              return resolve();
+            });
+        });
+      },
+      RegWriteExpandStringValue : (root,key,name,value) => {
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+            lib.RegWriteExpandStringValue.async(root,goPath(key),name,value.toString(), (err, res) => {
+              if(err) return reject(err);
+              return resolve();
+            });
+        });
+      },
+      RegWriteBinaryValue : (root,key,name,value) => {
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+            lib.RegWriteBinaryValue.async(root,goPath(key),name,value.toString(), (err, res) => {
+              if(err) return reject(err);
+              return resolve();
+            });
+        });
+      },
+      RegWriteDwordValue : (root,key,name,value) => {
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+            lib.RegWriteDwordValue.async(root,goPath(key),name,value.toString(), (err, res) => {
+              if(err) return reject(err);
+              return resolve();
+            });
+        });
+      },
+      RegWriteQwordValue : (root,key,name,value) => {
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+            lib.RegWriteQwordValue.async(root,goPath(key),name,value.toString(), (err, res) => {
+              if(err) return reject(err);
+              return resolve();
+            });
+        });
+      },
+      RegDeleteKey : (root,key) => {
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+            lib.RegDeleteKey.async(root,goPath(key), (err, res) => {
+              if(err) return reject(err);
+              return resolve();
+            });
+        });
+      },
+      RegDeleteKeyValue : (root,key,name) => {
+        return new Promise((resolve,reject) => {
+            if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+            lib.RegDeleteKeyValue.async(root,goPath(key),name, (err, res) => {
+              if(err) return reject(err);
+              return resolve();
+            });
+        });
       }
-      
-    } else {
-      return null;
+    },//Sync
+    RegKeyExists : (root,key) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        let res = lib.RegKeyExists(root,goPath(key));
+        return (res === 1) ? true : false;
+    },
+    RegListAllSubkeys : (root,key) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        let list = lib.RegListAllSubkeys(root,goPath(key));
+        if (list) {
+          let res = list.split(",");
+          return (res.length > 0) ? res : null;
+        } else {
+          return null;
+        }
+    },
+    RegListAllValues : (root,key) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        let list = lib.RegListAllValues(root,goPath(key));
+        if (list) {
+          let res = list.split(",");
+          if (res.length > 0) { 
+            return res.sort(function(a, b){ //Alphabetical sort to match regedit view
+                if(a < b) { return -1; }
+                if(a > b) { return 1; }
+                return 0;
+            })
+          } else {
+            return null;
+          }
+        } else {
+          return null;
+        }
+    },
+    RegQueryStringValue : (root,key,name) => { // REG_SZ & REG_EXPAND_SZ
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`; 
+        let res = lib.RegQueryStringValue(root,goPath(key),name);
+        return (res) ? res : null;
+    },
+    RegQueryStringValueAndExpand : (root,key,name) => { // REG_EXPAND_SZ (expands environment-variable strings)
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`; 
+        let res = lib.RegQueryStringValueAndExpand(root,goPath(key),name);
+        return (res) ? res : null;
+    },
+    RegQueryBinaryValue : (root,key,name) => { //REG_BINARY
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;  
+        let res = lib.RegQueryBinaryValue(root,goPath(key),name);
+        return (res) ? res : null;
+    },
+    RegQueryIntegerValue : (root,key,name) => { //REG_DWORD & REG_QWORD
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;   
+        let res = lib.RegQueryIntegerValue(root,goPath(key),name);
+        return (res) ? res : null;
+    },
+    RegWriteStringValue : (root,key,name,value) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        lib.RegWriteStringValue(root,goPath(key),name,value.toString());
+    },
+    RegWriteExpandStringValue : (root,key,name,value) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        lib.RegWriteExpandStringValue(root,goPath(key),name,value.toString());
+    },
+    RegWriteBinaryValue : (root,key,name,value) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        lib.RegWriteBinaryValue(root,goPath(key),name,value.toString());
+    },
+    RegWriteDwordValue : (root,key,name,value) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        lib.RegWriteDwordValue(root,goPath(key),name,value.toString());
+    },
+    RegWriteQwordValue : (root,key,name,value) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        lib.RegWriteQwordValue(root,goPath(key),name,value.toString());
+    },
+    RegDeleteKey : (root,key) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        lib.RegDeleteKey(root,goPath(key));
+    },
+    RegDeleteKeyValue : (root,key,name) => {
+        if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
+        lib.RegDeleteKeyValue(root,goPath(key),name);
     }
-}
-
-module.exports.RegQueryStringValue = function(root,key,name) { // REG_SZ & REG_EXPAND_SZ
-    
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-        
-    return regedit.RegQueryStringValue(root,goPath(key),name);
-}
-
-module.exports.RegQueryStringValueAndExpand = function(root,key,name) { // REG_EXPAND_SZ (expands environment-variable strings)
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-        
-    return regedit.RegQueryStringValueAndExpand (root,goPath(key),name);
-}
-
-module.exports.RegQueryBinaryValue = function(root,key,name) { //REG_BINARY
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-        
-    return regedit.RegQueryBinaryValue (root,goPath(key),name);
-}
-
-module.exports.RegQueryIntegerValue = function(root,key,name) { //REG_DWORD & REG_QWORD
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-        
-    return regedit.RegQueryIntegerValue (root,goPath(key),name);
-}
-
-module.exports.RegWriteStringValue = function(root,key,name,value) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    regedit.RegWriteStringValue(root,goPath(key),name,value.toString());
-}
-
-module.exports.RegWriteExpandStringValue = function(root,key,name,value) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    regedit.RegWriteExpandStringValue(root,goPath(key),name,value.toString());
-}
-
-module.exports.RegWriteBinaryValue = function(root,key,name,value) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    regedit.RegWriteBinaryValue(root,goPath(key),name,value.toString());
-}
-
-module.exports.RegWriteDwordValue = function(root,key,name,value) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    regedit.RegWriteDwordValue(root,goPath(key),name,value.toString());
-}
-
-module.exports.RegWriteQwordValue = function(root,key,name,value) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    regedit.RegWriteQwordValue(root,goPath(key),name,value.toString());
-}
-
-module.exports.RegDeleteKey = function(root,key) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    regedit.RegDeleteKey(root,goPath(key));
-}
-
-module.exports.RegDeleteKeyValue = function(root,key,name) {
-  
-    if (!HKEY.some(key => root === key)) throw `Unvalid root key "${root}"`;
-  
-    regedit.RegDeleteKeyValue(root,goPath(key),name);
-}
+};
