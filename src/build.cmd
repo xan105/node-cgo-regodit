04251403e67fb50ca622557cd518bbbb0bd84c79
Anthony Beaumont <dev.xan105@outlook.be>
2023-12-01 19:12:05 +0700

    update build script

--- src/build.cmd
+++ src/build.cmd
@@ -1,20 +1,18 @@
 @echo off
+cd %~dp0
 set CGO_ENABLED=1
 
-echo x64
+echo Compiling x64...
 set GOARCH=amd64
 set CC=zig cc -target x86_64-windows-gnu
 go build -buildmode=c-shared -o "%~dp0..\dist\regodit.x64.dll" regodit
-del /Q "%~dp0..\dist\regodit.x64.h"
 
-echo x86
+echo Compiling x86...
 set GOARCH=386
 set CC=zig cc -target x86-windows-gnu
 go build -buildmode=c-shared -o "%~dp0..\dist\regodit.x86.dll" regodit
-del /Q "%~dp0..\dist\regodit.x86.h"
 
-echo arm64
+echo Compiling arm64...
 set GOARCH=arm64
 set CC=zig cc -target aarch64-windows-gnu
 go build -buildmode=c-shared -o "%~dp0..\dist\regodit.arm64.dll" regodit
-del /Q "%~dp0..\dist\regodit.arm64.h"
