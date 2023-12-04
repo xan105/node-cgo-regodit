8b47b0c5e4978bf05b4ad629083368cc72780a98
Anthony Beaumont <dev.xan105@outlook.be>
2023-12-04 14:29:33 +0700

    types
    tired of TS bs; good enough; I'm tired

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,43 +1,23 @@
-export function regKeyExists(root: string,key: string): boolean
-export function regListAllSubkeys(root: string,key: string): any
-export function regListAllValues(root: string,key: string): any
-export function regQueryValueType(root: string,key: string,name: string): string
-export function regQueryStringValue(root: string,key: string,name: string): string | null
-export function regQueryMultiStringValue(root: string,key: string,name: string): string[] | null
-export function regQueryStringValueAndExpand(root: string,key: string,name: string): string | null
-export function regQueryBinaryValue(root: string,key: string,name: string): Buffer | null
-export function regQueryIntegerValue(root: string,key: string,name: string): number | bigint | null
-export function regWriteKey(root: string,key: string): void
-export function regWriteStringValue(root: string,key: string,name: string,value: string): void
-export function regWriteMultiStringValue(root: string,key: string,name: string,value: string[]): void
-export function regWriteExpandStringValue(root: string,key: string,name: string,value: string): void
-export function regWriteBinaryValue(root: string,key: string,name: string,value: Buffer): void
-export function regWriteDwordValue(root: string,key: string,name: string,value: number | bigint): void
-export function regWriteQwordValue(root: string,key: string,name: string,value: number | bigint): void
-export function regDeleteKeyValue(root: string,key: string,name: string): void
-export function regDeleteKey(root: string,key: string): void
+export function regKeyExists(root: string,key: string): boolean;
+export function regListAllSubkeys(root: string,key: string): string[];
+export function regListAllValues(root: string,key: string): string[];
+export function regQueryValueType(root: string,key: string,name: string): string;
+export function regQueryStringValue(root: string,key: string,name: string): string | null;
+export function regQueryMultiStringValue(root: string,key: string,name: string): string[] | null;
+export function regQueryStringValueAndExpand(root: string,key: string,name: string): string | null;
+export function regQueryBinaryValue(root: string,key: string,name: string): Buffer | null;
+export function regQueryIntegerValue(root: string,key: string,name: string): number | bigint | null;
+export function regWriteKey(root: string,key: string): void;
+export function regWriteStringValue(root: string,key: string,name: string,value: string): void;
+export function regWriteMultiStringValue(root: string,key: string,name: string,value: string[]): void;
+export function regWriteExpandStringValue(root: string,key: string,name: string,value: string): void;
+export function regWriteBinaryValue(root: string,key: string,name: string,value: Buffer): void;
+export function regWriteDwordValue(root: string,key: string,name: string,value: number | bigint): void;
+export function regWriteQwordValue(root: string,key: string,name: string,value: number | bigint): void;
+export function regDeleteKeyValue(root: string,key: string,name: string): void;
+export function regDeleteKey(root: string,key: string): void;
+export function regExportKey(root: string,key: string,option?: object): object;
+export function regImportKey(root: string,key: string,data: object, option?: object): void;
+export function regDeleteKeyIncludingSubkeys(root: string,key: string): void;
 
-declare interface IRegistryValues {
-  name: string,
-  type: string,
-  data: string | string[] | Buffer | number | bigint | null
-}
-
-declare interface IRegistry {
-  __values__?: IRegistryValues[]
-  [propName: string]: IRegistry;
-}
-
-declare interface IExportOpts {
-  recursive?: boolean
-}
-
-declare interface IImportOpts {
-  purgeDest?: boolean
-}
-
-export regExportKey(root: string,key: string,option?: IExportOpts): IRegistry
-export regImportKey(root: string,key: string,data: IRegistry, option?: IImportOpts): void
-export regDeleteKeyIncludingSubkeys(root: string,key: string): void
-
-export * as promises from "./promises.d.ts"
+export * as promises from "./promises.d.ts";
--- types/promises.d.ts
+++ types/promises.d.ts
@@ -1,41 +1,21 @@
-export function regKeyExists(root: string,key: string): Promise<boolean>
-export function regListAllSubkeys(root: string,key: string): Promise<any>
-export function regListAllValues(root: string,key: string): Promise<any>
-export function regQueryValueType(root: string,key: string,name: string): Promise<string>
-export function regQueryStringValue(root: string,key: string,name: string): Promise<string | null>
-export function regQueryMultiStringValue(root: string,key: string,name: string): Promise<string[] | null>
-export function regQueryStringValueAndExpand(root: string,key: string,name: string): Promise<string | null>
-export function regQueryBinaryValue(root: string,key: string,name: string): Promise<Buffer | null>
-export function regQueryIntegerValue(root: string,key: string,name: string): Promise<number | bigint | null>
-export function regWriteKey(root: string,key: string): Promise<void>
-export function regWriteStringValue(root: string,key: string,name: string,value: string): Promise<void>
-export function regWriteMultiStringValue(root: string,key: string,name: string,value: string[]): Promise<void>
-export function regWriteExpandStringValue(root: string,key: string,name: string,value: string): Promise<void>
-export function regWriteBinaryValue(root: string,key: string,name: string,value: Buffer): Promise<void>
-export function regWriteDwordValue(root: string,key: string,name: string,value: number | bigint): Promise<void>
-export function regWriteQwordValue(root: string,key: string,name: string,value: number | bigint): Promise<void>
-export function regDeleteKeyValue(root: string,key: string,name: string): Promise<void>
-export function regDeleteKey(root: string,key: string): Promise<void>
-
-declare interface IRegistryValues {
-  name: string,
-  type: string,
-  data: string | string[] | Buffer | number | bigint | null
-}
-
-declare interface IRegistry {
-  __values__?: IRegistryValues[]
-  [propName: string]: IRegistry;
-}
-
-declare interface IExportOpts {
-  recursive?: boolean
-}
-
-declare interface IImportOpts {
-  purgeDest?: boolean
-}
-
-export regExportKey(root: string,key: string,option?: IExportOpts): Promise<IRegistry>
-export regImportKey(root: string,key: string,data: IRegistry, option?: IImportOpts): Promise<void>
-export regDeleteKeyIncludingSubkeys(root: string,key: string): Promise<void>
+export function regKeyExists(root: string,key: string): Promise<boolean>;
+export function regListAllSubkeys(root: string,key: string): Promise<string[]>;
+export function regListAllValues(root: string,key: string): Promise<string[]>;
+export function regQueryValueType(root: string,key: string,name: string): Promise<string>;
+export function regQueryStringValue(root: string,key: string,name: string): Promise<string | null>;
+export function regQueryMultiStringValue(root: string,key: string,name: string): Promise<string[] | null>;
+export function regQueryStringValueAndExpand(root: string,key: string,name: string): Promise<string | null>;
+export function regQueryBinaryValue(root: string,key: string,name: string): Promise<Buffer | null>;
+export function regQueryIntegerValue(root: string,key: string,name: string): Promise<number | bigint | null>;
+export function regWriteKey(root: string,key: string): Promise<void>;
+export function regWriteStringValue(root: string,key: string,name: string,value: string): Promise<void>;
+export function regWriteMultiStringValue(root: string,key: string,name: string,value: string[]): Promise<void>;
+export function regWriteExpandStringValue(root: string,key: string,name: string,value: string): Promise<void>;
+export function regWriteBinaryValue(root: string,key: string,name: string,value: Buffer): Promise<void>;
+export function regWriteDwordValue(root: string,key: string,name: string,value: number | bigint): Promise<void>;
+export function regWriteQwordValue(root: string,key: string,name: string,value: number | bigint): Promise<void>;
+export function regDeleteKeyValue(root: string,key: string,name: string): Promise<void>;
+export function regDeleteKey(root: string,key: string): Promise<void>;
+export function regExportKey(root: string,key: string,option?: object): Promise<object>;
+export function regImportKey(root: string,key: string,data: object, option?: object): Promise<void>;
+export function regDeleteKeyIncludingSubkeys(root: string,key: string): Promise<void>;
